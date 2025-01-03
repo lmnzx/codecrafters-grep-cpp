38b19656b8ef374670b3d557f9d4eede7366b042
Sayan Mallick <snyxmk@gmail.com>
2025-01-03 23:26:08 +0530

    stage 1

--- src/Server.cpp
+++ src/Server.cpp
@@ -15,9 +15,6 @@ int main(int argc, char* argv[]) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cerr << "Logs from your program will appear here" << std::endl;
-
     if (argc != 3) {
         std::cerr << "Expected two arguments" << std::endl;
         return 1;
@@ -32,18 +29,18 @@ int main(int argc, char* argv[]) {
     }
 
     // Uncomment this block to pass the first stage
-    //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
-    //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+    
+    std::string input_line;
+    std::getline(std::cin, input_line);
+    
+     try {
+         if (match_pattern(input_line, pattern)) {
+             return 0;
+         } else {
+             return 1;
+         }
+     } catch (const std::runtime_error& e) {
+         std::cerr << e.what() << std::endl;
+         return 1;
+     }
 }
